d9f04ae604c007f84a99cfb2fafe28bd789b228d
악덕 <rudejrqkr97@gmail.com>
2024-06-20 17:35:50 +0900

    Quiz: [문제 4] 구구단 출력
    - 새로운 NestedEx1 파일 추가
    - main 함수에서 1부터 9까지의 구구단 출력
    - i와 j 변수를 사용하여 구구단 계산
    - 출력 형식은 "i * j = 결과값"
    - 각 단이 끝날 때마다 줄바꿈

---
+++ src/loop/ex/NestedEx1.java
@@ -0,0 +1,14 @@
+package loop.ex;
+
+public class NestedEx1 {
+    public static void main(String[] args) {
+
+        System.out.println("구구단 출력");
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + " * " + j + " = " + i * j);
+            }
+            System.out.println();
+        }
+    }
+}
