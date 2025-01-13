b3efd8fdc52b439beb231c121bb01dadfd7d437f
악덕 <rudejrqkr97@gmail.com>
2025-01-13 23:38:20 +0900

    생성자: this
    - MethodInitMain3 클래스에서는 MemberInit 객체를 생성하고, initMember 메소드를 호출하여 멤버를 초기화합니다.
    - MemberInit 클래스에 initMember 메소드를 추가하였습니다. 이 메소드는 이름, 나이, 성적을 입력받아 멤버를 초기화합니다.
    - MemberThis 클래스에서는 initMember 메소드를 통해 이름 필드를 초기화합니다. 이때, this 키워드는 생략 가능합니다.

---
+++ Java-Basic/src/construct/MemberThis.java
@@ -0,0 +1,11 @@
+package construct;
+
+public class MemberThis {
+    String nameField;
+
+    // this 생략 가능
+    void initMember(String nameParameter) {
+        nameField = nameParameter;
+    }
+
+}
