8ff26e5539513e29d3a425f0d325d8e6cbc0863a
악덕 <rudejrqkr97@gmail.com>
2025-01-13 23:52:43 +0900

    생성자: 오버로딩과 this()
    - MemberConstruct 클래스에 생성자 오버로딩을 추가하였습니다.
    - 이를 통해 사용자가 성적을 입력하지 않았을 경우, 기본값 50으로 설정되도록 하였습니다.
    - ConstructMain2 클래스를 새로 생성하여, 생성자 오버로딩이 정상적으로 작동하는지 테스트하였습니다.

--- Java-Basic/src/construct/MemberConstruct.java
+++ Java-Basic/src/construct/MemberConstruct.java
@@ -5,6 +5,11 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    // 추가(생성자 오버로딩)
+    MemberConstruct(String name, int age) {
+        this(name, age, 50); // 변경 - this()는 생성자 첫 줄에만 작성 가능
+    }
+
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name=" + name + ", age=" + age + ", grade=" + grade);
         this.name = name;
