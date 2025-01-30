6aa9f15ed0fa875440ae3cac8aaa48e60bab78df
악덕 <rudejrqkr97@gmail.com>
2025-01-30 18:11:11 +0900

    다형성과 설계: 다형성 - 역할과 구현 예제2
    - Driver 클래스에 Model3Car 인스턴스를 저장할 수 있는 필드 추가
    - Model3Car 인스턴스를 설정할 수 있는 setModel3Car 메소드 추가
    - drive 메소드에서 Model3Car 인스턴스가 있을 경우 Model3Car를 운전하도록 수정
    - Model3Car 클래스 생성
    - CarMain0에서 Model3Car 인스턴스 생성 후 Driver에 설정하여 운전 테스트 코드 추가

--- Java-Basic/src/poly/car0/CarMain0.java
+++ Java-Basic/src/poly/car0/CarMain0.java
@@ -7,5 +7,10 @@ public class CarMain0 {
 
         driver.setK3Car(k3Car);
         driver.drive();
+
+        Model3Car model3Car = new Model3Car();
+        driver.setK3Car(null);
+        driver.setModel3Car(model3Car);
+        driver.drive();
     }
 }
