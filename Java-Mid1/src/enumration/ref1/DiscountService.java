d4afdfea9fa4cc8a1f3b02f28d5ca8035658bb6f
악덕 <rudejrqkr97@gmail.com>
2025-02-10 00:15:02 +0900

    열거형 - ENUM: 열거형 - 리팩토링1
    - 'ClassGradeExRefMain1.java', 'ClassGrade.java', 'DiscountService.java' 파일 추가
    - 'ClassGrade' 클래스에서 BASIC, GOLD, DIAMOND 등급에 대한 할인율 설정
    - 'DiscountService' 클래스에서 각 등급에 따른 할인 금액 계산 기능 구현
    - 'ClassGradeExRefMain1' 클래스에서 할인 서비스 테스트 코드 작성

---
+++ Java-Mid1/src/enumration/ref1/DiscountService.java
@@ -0,0 +1,7 @@
+package enumration.ref1;
+
+public class DiscountService {
+    public int discount(ClassGrade classGrade, int price) {
+        return price * classGrade.getDiscountPercent() / 100;
+    }
+}
