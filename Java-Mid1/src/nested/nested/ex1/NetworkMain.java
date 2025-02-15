ed503f5c151c731308a4a406fe768c9ff457c157
악덕 <rudejrqkr97@gmail.com>
2025-02-15 23:58:30 +0900

    중첩 클래스, 내부 클래스1: 정적 충첩 클래스 활용
    - NetWork 클래스와 NetworkMessage 클래스를 추가하여 메시지 전송 기능 구현
    - sendMessage 메소드를 통해 텍스트 메시지를 NetworkMessage 객체로 전송
    - NetworkMessage 클래스는 메시지 내용을 출력하는 print 메소드 포함
    - NetworkMain 클래스에서는 NetWork 객체를 생성하고 sendMessage 메소드를 호출하여 메시지 전송 테스트

---
+++ Java-Mid1/src/nested/nested/ex1/NetworkMain.java
@@ -0,0 +1,9 @@
+package nested.nested.ex1;
+
+public class NetworkMain {
+    public static void main(String[] args) {
+        NetWork netWork = new NetWork();
+        netWork.sendMessage("hello java");
+    }
+
+}
