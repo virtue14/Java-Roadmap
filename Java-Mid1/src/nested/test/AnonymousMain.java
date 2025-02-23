fbbdc08a691151c3993caeb9dfaf0616ca7a72cf
악덕 <rudejrqkr97@gmail.com>
2025-02-23 23:33:37 +0900

    중첩 클래스, 내부 클래스2: 문제와 풀이4 - 익명 클래스를 완성해라
    - 'AnonymousMain.java' 파일과 'Hello.java' 인터페이스 생성
    - 'Hello' 인터페이스에 'hello' 메소드 추가
    - 'AnonymousMain' 클래스에서 'Hello'의 익명 클래스 생성 및 'hello' 메소드 호출 기능 추가

---
+++ Java-Mid1/src/nested/test/AnonymousMain.java
@@ -0,0 +1,14 @@
+package nested.test;
+
+public class AnonymousMain {
+    public static void main(String[] args) {
+    // 여기에서 Hello의 익명 클래스를 생성하고 hello()를 호출해라.
+        Hello hello = new Hello() {
+            @Override
+            public void hello() {
+                System.out.println("Hello.hello");
+            }
+        };
+        hello.hello();
+    }
+}
